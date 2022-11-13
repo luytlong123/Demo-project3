a1a510fd368e10610b021b1abb837023abeb4eff
tranquocnha <tranquocnha1808000@gmail.com>
2022-11-13 17:20:28 +0700

    a

---
+++ project/set_env.sh
@@ -0,0 +1,12 @@
+# # This file is used for convenience of local development.
+# # DO NOT STORE YOUR CREDENTIALS INTO GIT
+
+export POSTGRES_USERNAME = postgres
+export POSTGRES_PASSWORD =  mypassword
+export POSTGRES_HOST = database-1.c6dtc3ezn7cv.us-east-1.rds.amazonaws.com
+export POSTGRES_DB = postgres
+export AWS_BUCKET= nhatqproject3
+export AWS_REGION= us-west-2
+export AWS_PROFILE= default
+export JWT_SECRET= testing
+export URL= http://localhost:8100
